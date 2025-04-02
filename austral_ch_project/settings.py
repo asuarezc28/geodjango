8f68cbfc254493c947cb08f2cfae5ac160f5ae02
Ángel Suárez <asuarezc91@gmail.com>
2025-04-02 23:46:31 +0200

    refactor: update database configuration in settings to use dj_database_url.config for improved connection handling and enable SSL mode for enhanced security

--- austral_ch_project/settings.py
+++ austral_ch_project/settings.py
@@ -71,10 +71,14 @@ WSGI_APPLICATION = 'austral_ch_project.wsgi.application'
 
 # Base de datos: usa DATABASE_URL si existe, sino PostGIS local
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv('DATABASE_URL', 'postgres://postgres:1234@localhost:5432/austral_ch_project'),
+    'default': dj_database_url.config(
+        default='postgres://postgres:1234@localhost:5432/austral_ch_project',
         conn_max_age=600,
-        engine='django.contrib.gis.db.backends.postgis'
+        engine='django.contrib.gis.db.backends.postgis',
+        ssl_require=True,
+        options={
+            'sslmode': 'require',
+        }
     )
 }
 
