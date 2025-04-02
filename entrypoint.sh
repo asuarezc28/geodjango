df061a3899b2fb97827312503f422d07b3a5ffdb
Ángel Suárez <asuarezc91@gmail.com>
2025-04-02 19:44:15 +0200

    fix: update entrypoint script to set environment variables and improve Gunicorn startup command

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,10 +24,12 @@ echo ">>> Aplicando migraciones..."
 python manage.py migrate
 python manage.py create_admin
 
-echo ">>> Levantando Gunicorn..."
-exec gunicorn austral_ch_project.wsgi:application --bind 0.0.0.0:8000
-
-
-echo "== Ejecutando servidor con DEBUG =="
+echo "== Levantando Gunicorn =="
+export DJANGO_SETTINGS_MODULE=austral_ch_project.settings
 export DJANGO_DEBUG=True
-exec gunicorn austral_ch_project.wsgi:application --bind 0.0.0.0:8000 --log-level debug
+
+exec gunicorn austral_ch_project.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --log-level debug
+
+
