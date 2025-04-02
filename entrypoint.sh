9cc37f1f8fcb530370a792ebbaf816fff22b3fcc
Ángel Suárez <asuarezc91@gmail.com>
2025-04-02 23:08:10 +0200

    refactor: update entrypoint script to increase database wait time, ensure correct directory, and modify migration steps for improved deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# Esperar a que la DB esté lista (opcional)
+# Esperar a que la DB esté lista
 echo "Esperando a la base de datos..."
-sleep 5
+sleep 15  # Aumentamos el tiempo de espera
 
-# Recolectar archivos estáticos (para que funcione el admin en producción)
+# Asegurarnos de que estamos en el directorio correcto
+cd /app
+
+# Recolectar archivos estáticos
 echo ">>> Ejecutando collectstatic..."
 python manage.py collectstatic --noinput
 
 # Aplicar migraciones y crear superusuario
 echo ">>> Aplicando migraciones..."
-python manage.py migrate
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 python manage.py create_admin
 
-# Levantar Gunicorn en modo debug para registrar errores
+# Levantar Gunicorn en modo debug
 echo "== Levantando Gunicorn =="
 export DJANGO_SETTINGS_MODULE=austral_ch_project.settings
 export DJANGO_DEBUG=True
